8b308e1787552d3da765203d50498b48603c4669
NORMAND, Thibault <thibault.normand@continental-corporation.com>
2019-04-26 16:55:57 +0200

    chore(ci): Vendoring tools before installation.

--- tools/magefile.go
+++ tools/magefile.go
@@ -15,8 +15,15 @@ var deps = []string{
 	"github.com/izumin5210/gex/cmd/gex",
 }
 
+// Vendor create tools vendors
+func (Go) Vendor() error {
+	fmt.Println("## Vendoring dependencies")
+	return sh.RunV("go", "mod", "vendor")
+}
+
 // Tools updates tools from package
 func (Go) Tools() error {
+	mg.SerialDeps(Go.Vendor)
 	fmt.Println("## Intalling tools")
 	return sh.RunV("go", "run", "github.com/izumin5210/gex/cmd/gex", "--build")
 }
